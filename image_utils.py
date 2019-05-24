c2521a2fd75092c98f3dc1e0b06468bee659e74a
Ishmael <darkflare1055@gmail.com>
2019-05-24 22:11:03 +0800

    image_utils

--- image_utils.py
+++ image_utils.py
@@ -14,14 +14,14 @@ def crop_border(img, output_path, default_size=None):
         else:
             assert len(default_size) == 2, default_size
             x_min, y_min, x_max, y_max = 0, 0, default_size[0], default_size[1]
-            old_im = old_im.crop((x_min, y_min, x_max+1, y_max+1))
+            old_im = old_im.crop((x_min, y_min, x_max + 1, y_max + 1))
             old_im.save(output_path)
             return False
     y_min = np.min(nnz_inds[0])
     y_max = np.max(nnz_inds[0])
     x_min = np.min(nnz_inds[1])
     x_max = np.max(nnz_inds[1])
-    old_im = old_im.crop((x_min, y_min, x_max+1, y_max+1))
+    old_im = old_im.crop((x_min, y_min, x_max + 1, y_max + 1))
     old_im.save(output_path)
     return True
 
@@ -39,9 +39,9 @@ def crop_lines(im):
     lines_index_start = [0]
     lines_index_end = []
     for i in range(1, len(im_sum)):
-        if im_sum[i] == 0 and im_sum[i-1] != 0:
+        if im_sum[i] == 0 and im_sum[i - 1] != 0:
             lines_index_end.append(i)
-        elif im_sum[i] != 0 and im_sum[i-1] == 0:
+        elif im_sum[i] != 0 and im_sum[i - 1] == 0:
             lines_index_start.append(i)
     lines_index_end.append(lin)
 
@@ -49,15 +49,16 @@ def crop_lines(im):
     for i in range(len(lines_index_start)):
         line_mid = (lines_index_end[i] + lines_index_start[i]) // 2
         line_height = lines_index_end[i] - lines_index_start[i]
-        im_lines.append([lines_index_start[i], lines_index_end[i],
-                         line_mid, line_height,
-                         im_sum[line_mid], im_quarter_sum[line_mid]])
+        im_lines.append([
+            lines_index_start[i], lines_index_end[i], line_mid, line_height,
+            im_sum[line_mid], im_quarter_sum[line_mid]
+        ])
     i = 0
     lines = len(im_lines)
 
     heights = [im_lines[i][3] for i in range(lines)]
     height_mean = np.mean(heights)
-    while(i < lines):
+    while (i < lines):
         if im_lines[i][3] > 1.25 * height_mean or \
                 im_lines[i][4] < 0.02 or \
                 im_lines[i][5] < 0.02:
@@ -84,9 +85,9 @@ def crop_words(im):
         blank_start.append(0)
     blank_end = []
     for i in range(1, len(im_sum)):
-        if im_sum[i] != 0 and im_sum[i-1] == 0:
+        if im_sum[i] != 0 and im_sum[i - 1] == 0:
             blank_end.append(i)
-        elif im_sum[i] == 0 and im_sum[i-1] != 0:
+        elif im_sum[i] == 0 and im_sum[i - 1] != 0:
             blank_start.append(i)
     if im_sum[-1] == 0:
         blank_end.append(col)
@@ -95,8 +96,8 @@ def crop_words(im):
     for i in range(len(blank_start)):
         blank_mid = (blank_start[i] + blank_end[i]) // 2
         blank_width = blank_end[i] - blank_start[i]
-        im_blanks.append([blank_start[i], blank_end[i],
-                          blank_mid, blank_width])
+        im_blanks.append(
+            [blank_start[i], blank_end[i], blank_mid, blank_width])
 
     wids = [im_blanks[i][3] for i in range(len(im_blanks))]
     max_width_index = wids.index(max(wids))
@@ -135,8 +136,8 @@ def crop_words(im):
         im_words.append(col)
 
     for i in range(len(im_words) - 1):
-        word_im = im.crop((im_words[i], 0, im_words[i+1], lin))
-        word_im.save("word-" + str(i+1) + ".png")
+        word_im = im.crop((im_words[i], 0, im_words[i + 1], lin))
+        word_im.save("word-" + str(i + 1) + ".png")
 
     return
 
@@ -144,8 +145,8 @@ def crop_words(im):
 def pad_goup_image(img, output_path, pad_size, buckets):
     PAD_TOP, PAD_LEFT, PAD_BOTTOM, PAD_RIGHT = pad_size
     old_im = Image.open(img)
-    old_size = (old_im.size[0]+PAD_LEFT+PAD_RIGHT,
-                old_im.size[1]+PAD_TOP+PAD_BOTTOM)
+    old_size = (old_im.size[0] + PAD_LEFT + PAD_RIGHT,
+                old_im.size[1] + PAD_TOP + PAD_BOTTOM)
     j = -1
     for i in range(len(buckets)):
         if old_size[0] <= buckets[i][0] and old_size[1] <= buckets[i][1]:
@@ -170,7 +171,7 @@ def downsample_image(img, output_path, ratio):
         return True
     old_im = Image.open(img)
     old_size = old_im.size
-    new_size = (int(old_size[0]/ratio), int(old_size[1]/ratio))
+    new_size = (int(old_size[0] / ratio), int(old_size[1] / ratio))
 
     new_im = old_im.resize(new_size, PIL.Image.LANCZOS)
     new_im.save(output_path)
